e2ea9db322a89ac166ea3a1238f9929f52ad2e11
Ederson Araujo <siulloong@gmail.com>
2023-04-23 14:29:18 -0300

    Poliformismo aula 13 230423

---
+++ NetBeansProjects/Aula13_Poliformismo/src/aula13_poliformismo/Lobo.java
@@ -0,0 +1,10 @@
+
+package aula13_poliformismo;
+
+public class Lobo extends Mamifero{
+    @Override
+    public void emitirSom() {
+        System.out.println("Auuuuuuuuu");
+    }
+           
+}
