8465f224abf826908bd183ade4a9d189adaf5910
EDERSON <76922877+Siulloong@users.noreply.github.com>
2023-06-16 17:20:40 -0300

    Udemi 160623

---
+++ EclipseProjects/Udemi/Java/Aulas Java Udemi/src/PraticandoUdemiJava/Split.java
@@ -0,0 +1,20 @@
+package PraticandoUdemiJava;
+
+/*
+ * separa o texto usando o caracter indicado no split(" ") - aqui no caso o espaço
+ */
+
+public class Split {
+
+	public static void main(String[] args) {
+		String s = "Ederson da Cruz Araujo";
+		String[] vect = s.split(" ");
+		
+		System.out.println(vect[0]);
+		System.out.println(vect[1]);
+		System.out.println(vect[2]);
+		System.out.println(vect[3]);
+
+	}
+
+}
